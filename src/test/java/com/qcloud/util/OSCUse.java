3c6cdb3b012afc247cb349ebd18e34910e5781cb
yuanzhiqiang <965541921@qq.com>
2021-12-29 23:15:27 +0800

    1：优化

---
+++ src/test/java/com/qcloud/util/OSCUse.java
@@ -0,0 +1,27 @@
+package com.qcloud.util;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+
+//import osc.util.Constants;
+
+public class OSCUse {
+
+	public static void test() {
+//		try {
+//			TransferDownloadObject.commonPublishDownload("");
+//		} catch (NoSuchAlgorithmException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+	
+}
